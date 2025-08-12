b8dd4c97c3ce684e1e9f31af3182337372484923
laFlorNegra <1230112@isep.ipp.pt>
2025-08-12 18:55:01 +0200

    fix: dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
-          root: ['.'],
-          alias: {
-            'src': './src',
-          },
-        },
-      ],
-    ],
+  plugins: [],
   };
 };
